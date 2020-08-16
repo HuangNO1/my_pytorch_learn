bcaaf44046588eb532764189e88f0c7f92a83418
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-08-16 22:26:53 +0800

    Operations

--- 60mins/what_is_pytorch.py
+++ 60mins/what_is_pytorch.py
@@ -1,3 +1,5 @@
+# https://pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html
+
 # What is PyTorch?
 # It’s a Python-based scientific computing package targeted at two sets of audiences:
 
@@ -12,10 +14,107 @@
 from __future__ import print_function
 import torch
 
-# An uninitialized matrix is declared, but does not contain definite known values before it is used. When an uninitialized matrix is created,
+# NOTE
+# An uninitialized matrix is declared, but does not contain
+#  definite known values before it is used.
+#  When an uninitialized matrix is created,
 #  whatever values were in the allocated memory at the time will appear as the initial values.
 
 # Construct a 5x3 matrix, uninitialized:
 
 x = torch.empty(5, 3)
+print("\nx = torch.empty(5, 3)")
 print(x)
+
+# Construct a randomly initialized matrix:
+
+x = torch.rand(5, 3)
+print("\nx = torch.rand(5, 3)")
+print(x)
+
+# Construct a matrix filled zeros and of dtype long:
+
+x = torch.zeros(5, 3, dtype=torch.long)
+print("\nx = torch.zeros(5, 3, dtype=torch.long)")
+print(x)
+
+# Construct a tensor directly from data:
+
+x = torch.tensor([5.5, 3])
+print("\nx = torch.tensor([5.5, 3])")
+print(x)
+
+# or create a tensor based on an existing tensor.
+#  These methods will reuse properties of the input tensor,
+#  e.g. dtype, unless new values are provided by user
+
+x = x.new_ones(5, 3, dtype=torch.double)      # new_* methods take in sizes
+print("\nx = x.new_ones(5, 3, dtype=torch.double)")
+print(x)
+
+x = torch.randn_like(x, dtype=torch.float)    # override dtype!
+print("\nx = torch.randn_like(x, dtype=torch.float)")
+print(x)
+
+# Get its size:
+
+print("\nx.size()")
+print(x.size())
+
+# NOTE
+# torch.Size is in fact a tuple, so it supports all tuple operations.
+
+
+# Operations
+# There are multiple syntaxes for operations.
+#  In the following example, we will take a look at the addition operation.
+
+# Addition: syntax 1
+
+y = torch.rand(5, 3)
+print("\nx + y")
+print(x + y)
+
+# Addition: syntax 2
+
+print("\ntorch.add(x, y)")
+print(torch.add(x, y))
+
+# Addition: providing an output tensor as argument
+
+result = torch.empty(5, 3)
+torch.add(x, y, out=result)
+print("\ntorch.add(x, y, out=result) / print(result)")
+print(result)
+
+# Addition: in-place
+
+# adds x to y
+y.add_(x)
+print("\ny.add_(x)")
+print(y)
+
+# NOTE
+# Any operation that mutates a tensor in-place is post-fixed with an _.
+#  For example: x.copy_(y), x.t_(), will change x.
+
+# You can use standard NumPy-like indexing with all bells and whistles!
+
+print("\nx[:, 1]")
+print(x[:, 1])
+
+# Resizing: If you want to resize/reshape tensor, you can use torch.view:
+
+x = torch.randn(4, 4)
+y = x.view(16)
+z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
+print("\nview() -> x.size(), y.size(), z.size()")
+print(x.size(), y.size(), z.size())
+
+# If you have a one element tensor, use .item() to get the value as a Python number
+
+x = torch.randn(1)
+print("\nx")
+print(x)
+print("\nx.item()")
+print(x.item())
