fab67a4a72e8d4ef35055bc7b7e3fab429f2028c
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-08-18 00:53:39 +0800

    update

--- 60mins/autograd.py
+++ 60mins/autograd.py
@@ -76,3 +76,51 @@ print(b.grad_fn)
 
 
 # 2. Gradients
+
+# Let’s backprop now. Because out contains a single scalar,
+#  out.backward() is equivalent to out.backward(torch.tensor(1.)).
+
+out.backward()
+
+# Print gradients d(out)/dx
+print("\nout.backward()\nx.grad")
+print(x.grad)
+
+# You should have got a matrix of 4.5. Let’s call the out Tensor “o”.
+#  We have that o=14∑izi, zi=3(xi+2)2 and zi∣∣xi=1=27. Therefore,
+#  ∂o∂xi=32(xi+2), hence ∂o∂xi∣∣xi=1=92=4.5.
+
+# Now let’s take a look at an example of vector-Jacobian product:
+
+x = torch.randn(3, requires_grad=True)
+
+y = x * 2
+while y.data.norm() < 1000:
+    y = y * 2
+
+print(y)
+
+# Now in this case y is no longer a scalar. torch.autograd could not compute the full Jacobian directly,
+#  but if we just want the vector-Jacobian product, simply pass the vector to backward as argument:
+
+v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+y.backward(v)
+
+print(x.grad)
+
+# You can also stop autograd from tracking history on
+#  Tensors with .requires_grad=True either by wrapping the
+#  code block in with torch.no_grad():
+
+print(x.requires_grad)
+print((x ** 2).requires_grad)
+
+with torch.no_grad():
+    print((x ** 2).requires_grad)
+
+# Or by using .detach() to get a new Tensor with the same content but that does not require gradients:
+
+print(x.requires_grad)
+y = x.detach()
+print(y.requires_grad)
+print(x.eq(y).all())
