8962c42158e6cf4104db2538543cf60ece91897b
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-08-17 23:52:40 +0800

    tensor

--- 60mins/autograd.py
+++ 60mins/autograd.py
@@ -37,3 +37,42 @@
 #  arguments to backward(), however if it has more elements,
 #  you need to specify a gradient argument that is a tensor of matching shape.
 
+import torch
+
+# Create a tensor and set requires_grad=True to track computation with it
+
+x = torch.ones(2, 2, requires_grad=True)
+print("\nx = torch.ones(2, 2, requires_grad=True)")
+print(x)
+
+# Do a tensor operation:
+
+y = x + 2
+print("\ny = x + 2")
+print(y)
+
+# y was created as a result of an operation, so it has a grad_fn.
+
+print("\ny.grad_fn")
+print(y.grad_fn)
+
+# Do more operations on y
+
+z = y * y * 3
+out = z.mean()
+
+print("\nz = y * y * 3\nout = z.mean()")
+print(z, out)
+
+# .requires_grad_( ... ) changes an existing Tensor’s requires_grad flag in-place. The input flag defaults to False if not given.
+
+a = torch.randn(2, 2)
+a = ((a * 3) / (a - 1))
+print(a.requires_grad)
+a.requires_grad_(True)
+print(a.requires_grad)
+b = (a * a).sum()
+print(b.grad_fn)
+
+
+# 2. Gradients
