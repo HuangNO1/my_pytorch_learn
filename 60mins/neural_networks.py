9e032076c6495549771cd150bfe8a0b77f2b0a9e
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-08-19 00:06:46 +0800

    update

--- 60mins/neural_networks.py
+++ 60mins/neural_networks.py
@@ -24,3 +24,102 @@
 #     Update the weights of the network, typically using a simple update rule: weight = weight - learning_rate * gradient
 
 
+# 2. Define the network
+
+# Let’s define this network:
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 3x3 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+net = Net()
+print(net)
+
+# You just have to define the forward function, and the backward
+#  function (where gradients are computed) is automatically defined
+#  for you using autograd. You can use any of the Tensor operations in the forward function.
+
+# The learnable parameters of a model are returned by net.parameters()
+
+params = list(net.parameters())
+print(len(params))
+print(params[0].size())  # conv1's .weight
+
+# Let’s try a random 32x32 input. Note: expected input size of this net (LeNet) is 32x32.
+#  To use this net on the MNIST dataset, please resize the images from the dataset to 32x32.
+
+input = torch.randn(1, 1, 32, 32)
+out = net(input)
+print(out)
+
+# Zero the gradient buffers of all parameters and backprops with random gradients:
+
+net.zero_grad()
+out.backward(torch.randn(1, 10))
+
+
+
+# Note
+# 
+# torch.nn only supports mini-batches. The entire torch.nn package only supports inputs that are a mini-batch of samples, and not a single sample.
+# 
+# For example, nn.Conv2d will take in a 4D Tensor of nSamples x nChannels x Height x Width.
+# 
+# If you have a single sample, just use input.unsqueeze(0) to add a fake batch dimension.
+
+
+# Before proceeding further, let’s recap all the classes you’ve seen so far.
+# 
+# Recap:
+# 
+        # torch.Tensor - A multi-dimensional array with support for autograd operations like backward(). Also holds the gradient w.r.t. the tensor.
+        # nn.Module - Neural network module. Convenient way of encapsulating parameters, with helpers for moving them to GPU, exporting, loading, etc.
+        # nn.Parameter - A kind of Tensor, that is automatically registered as a parameter when assigned as an attribute to a Module.
+        # autograd.Function - Implements forward and backward definitions of an autograd operation. Every Tensor operation creates at least a single Function node
+        #  that connects to functions that created a Tensor and encodes its history.
+# 
+# At this point, we covered:
+# 
+        # Defining a neural network
+        # Processing inputs and calling backward
+# 
+# Still Left:
+# 
+        # Computing the loss
+        # Updating the weights of the network
+
+
+
